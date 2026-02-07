1b7a01570864769df74512b6a80c580aa2ed459b
ErgonComun <ergon.comunicacao.atendimento@gmail.com>
2026-02-07 20:27:01 -0300

    header removed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import OurStory from './components/OurStory';
 function App() {
   return (
     <div className="min-h-screen bg-wedding-white font-sans text-text-dark selection:bg-champagne-gold selection:text-white">
-      <Header />
+      {/* <Header /> */}
       <main>
         <Hero />
         <OurStory />
