10abd68507f704666c1ea9b1a16c9e130f61e6b8
ErgonComun <ergon.comunicacao.atendimento@gmail.com>
2026-02-07 19:57:39 -0300

    IOS bug

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'safari >= 12', 'iOS >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   build: {
     target: ['es2015', 'safari12'],
   },
