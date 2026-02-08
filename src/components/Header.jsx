b18432e4e7f913181eb839b18123a1b8dac639b3
ErgonComun <ergon.comunicacao.atendimento@gmail.com>
2026-02-07 21:12:16 -0300

    text optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
 
     return (
         <header className="fixed top-0 left-0 right-0 z-50 px-4  md:px-6 flex items-center justify-between">
-            <a href="#" className="font-serif text-2xl font-semibold text-text-dark tracking-wide">
+            <a href="#" className="">
                 Sarah <span className="text-champagne-gold">&</span> Nelson
             </a>
             {/* Desktop Nav */}
